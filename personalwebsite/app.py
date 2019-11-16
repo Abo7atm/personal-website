1a9cd23131bd1fd6344609ddebce88ca186530d3
abo7atm <aaalnumay@gmail.com>
2019-11-16 13:34:19 +0300

    simple webpage

--- personalwebsite/app.py
+++ personalwebsite/app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+app.config['ENV'] = 'development'
+app.config['DEBUG'] = True
+
 @app.route('/')
 def home():
-    return 'Hello there'
+    return render_template('index.html')
 
 if __name__=='__main__':
     app.run()
